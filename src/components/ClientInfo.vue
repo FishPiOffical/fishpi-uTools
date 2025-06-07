5307fe70e84247530eb7f737a264b33d04efdc90
刘绪 <1427868463@qq.com>
2025-06-07 22:54:18 +0800

    refactor(components): 精简 ClientInfo 组件中的来源信息文本
    - 移除了所有来源信息中的"来自"二字，使文本更加简洁
    - 统一了客户端、扩展程序等表述方式，提高了文本的一致性

--- src/components/ClientInfo.vue
+++ src/components/ClientInfo.vue
@@ -29,109 +29,109 @@ const vias = new Map([
   [
     "Windows",
     {
-      content: "来自桌面客户端",
+      content: "桌面客户端",
       url: "https://github.com/imlinhanchao/fishpi-desktop/releases",
     },
   ],
   [
     "macOS",
     {
-      content: "来自桌面客户端",
+      content: "桌面客户端",
       url: "https://github.com/imlinhanchao/fishpi-desktop/releases",
     },
   ],
   [
     "Linux",
     {
-      content: "来自桌面客户端",
+      content: "桌面客户端",
       url: "https://github.com/imlinhanchao/fishpi-desktop/releases",
     },
   ],
   [
     "Chrome",
     {
-      content: "来自 Chrome 扩展",
+      content: "Chrome 扩展",
       url: "https://chrome.google.com/webstore/detail/fkaomdjjdbglkbcmfhhlioejkpacbbpe",
     },
   ],
   [
     "Edge",
     {
-      content: "来自 Edge 扩展",
+      content: "Edge 扩展",
       url: "https://microsoftedge.microsoft.com/addons/detail/oldbilakhdpiamjbkocdcdnlnakainfm",
     },
   ],
   [
     "VSCode",
     {
-      content: "来自 VsCode 扩展",
+      content: "VsCode 扩展",
       url: "https://marketplace.visualstudio.com/items?itemName=hancel.pwl-chat",
     },
   ],
   [
     "IDEA",
     {
-      content: "来自 IDEA 扩展",
+      content: "IDEA 扩展",
       url: "https://plugins.jetbrains.com/plugin/18091-pwl-chat",
     },
   ],
   [
     "Python",
     {
-      content: "来自 Python 客户端",
+      content: "Python 客户端",
       url: `${BASE_URL}/article/1641135630423`,
     },
   ],
   [
     "Golang",
     {
-      content: "来自 Golang 客户端",
+      content: "Golang 客户端",
       url: `${BASE_URL}/article/1641661864119`,
     },
   ],
   [
     "Web",
     {
-      content: "来自 Web",
+      content: "Web",
       url: `${BASE_URL}/cr`,
     },
   ],
   [
     "iOS",
     {
-      content: "来自 iPhone 客户端",
+      content: "iPhone 客户端",
       url: "https://apps.apple.com/cn/app/%E6%91%B8%E9%B1%BC%E6%B4%BE/id1617385824",
     },
   ],
   [
     "Android",
     {
-      content: "来自 Android 客户端",
+      content: "Android 客户端",
       url: `${BASE_URL}/article/1641291342622`,
     },
   ],
   [
     "Mobile",
     {
-      content: "来自移动端",
+      content: "移动端",
     },
   ],
   [
     "Extension",
     {
-      content: "来自扩展程序",
+      content: "扩展程序",
     },
   ],
   [
     "PC",
     {
-      content: "来自桌面客户端",
+      content: "桌面客户端",
     },
   ],
   [
     "Other",
     {
-      content: "来自其他客户端",
+      content: "其他客户端",
     },
   ],
 ]);
